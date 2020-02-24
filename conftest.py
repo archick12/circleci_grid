c4e9f5ce0a75138b024b03a61df5414b14397603
apiliuk <lohika.artur@98point6.com>
2020-02-24 14:03:40 +0200

    added fixture

--- conftest.py
+++ conftest.py
@@ -2,13 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
-
+import socket
 
 @pytest.fixture(scope="function")
 def browser():
    # browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    grid_url = "http://localhost:4444/wd/hub"
-    desired_caps = DesiredCapabilities.CHROME
-    browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
-    yield browser
-    browser.quit()
+   host_name = socket.gethostname()
+   host_ip = socket.gethostbyname(host_name)
+   grid_url = "http://" + host_ip +":4444/wd/hub"
+   desired_caps = DesiredCapabilities.CHROME
+   browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
+   yield browser
+   browser.quit()
