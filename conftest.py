28e46b4e22820fdc0686d1b9238ea5fe1681309e
apiliuk <lohika.artur@98point6.com>
2020-02-24 15:47:23 +0200

    added ip address

--- conftest.py
+++ conftest.py
@@ -2,14 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
-import socket
+import os
 
 @pytest.fixture(scope="function")
 def browser():
    # browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
 
-   grid_url = "http://127.0.0.1:4444/wd/hub"
+   print("ENVIRONMENT VARIABLE " + os.environ['TEST_IP_ADDRESS'])
+   grid_url = "http://" + os.environ['TEST_IP_ADDRESS'] + ":4444/wd/hub"
    desired_caps = DesiredCapabilities.CHROME
    browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
    yield browser
